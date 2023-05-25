5a2320b59f7a44590e13f9e3525d8443ede995c2
SayanthD <13906889+SayanthD@users.noreply.github.com>
2023-05-25 17:22:33 +0530

    Set link to source message in caption

--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
-/* MIT License
-
-Copyright (c) 2023 SayanthD
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE. */
+/** 
+ * MIT License
+ * 
+ * Copyright (c) 2023 SayanthD
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ */
 
 import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
@@ -69,7 +71,9 @@ async function getReported(event: NewMessageEvent) {
         console.log(`reported text: ${message.message}`);
         const reportedMedia = message.replyTo ? (await client.getMessages(sourceChat, { ids: message.replyTo.replyToMsgId }))[0] : null;
         if (reportedMedia && reportedMedia.media) {
-            await sendMedia(reportedMedia.media, `this was reported. Reason: ${message.message}, ID: ${message.id}`);
+            const chatId = message.chat?.id.toString();
+            const customCaption = `this was reported. Reason: ${message.message}, ID: <a href='https://t.me/c/${chatId}/${message.id}'>${message.id}</a>`
+            await sendMedia(reportedMedia.media, customCaption);
         };
     } catch (err) {
         console.error(err);
@@ -80,8 +84,14 @@ async function getMedia(event: NewMessageEvent) {
     const message = event.message as Api.Message;
     if (!message.media) return;
     try {
+        const chatId = message.chat?.id.toString();
+        const caption = message.text || "";
         console.log(`New ${message.media.className} with id: ${message.id}`);
-        await sendMedia(message.media, `${message.id}`);
+        let customCaption = `<a href='https://t.me/c/${chatId}/${message.id}'>${message.id}</a>`
+        if (caption) {
+            customCaption += ` Caption: ${caption}`
+        }
+        await sendMedia(message.media, customCaption);
     }
     catch (error) {
         console.error(error)
@@ -91,14 +101,21 @@ async function getMedia(event: NewMessageEvent) {
 const client = new TelegramClient(
     stringSession, apiId,
     apiHash,
-    { connectionRetries: 5 });
+    { connectionRetries: 5, floodSleepThreshold: 180 });
 
 (async () => {
     await client.start({ botAuthToken: "" });
-    const chatToMonitor = await client.getEntity(sourceChat);
-    client.addEventHandler(getReported, new NewMessage({ chats: [chatToMonitor.id], pattern: rgxMatches }));
-    client.addEventHandler(getMedia, new NewMessage({ chats: [chatToMonitor.id] }));
+    try {
+        const chatToMonitor = await client.getEntity(sourceChat);
 
+        client.addEventHandler(getReported, new NewMessage({ chats: [chatToMonitor.id], pattern: rgxMatches }));
+        client.addEventHandler(getMedia, new NewMessage({ chats: [chatToMonitor.id] }));
+    }
+    catch (error) {
+        throw new Error("Invalid chat given!");
+    }
+
+    client.setParseMode("html");
     const me = await client.getMe() as Api.User;
     console.log(`${me.username} started with GramJS ${client.__version__}`);
 })();
